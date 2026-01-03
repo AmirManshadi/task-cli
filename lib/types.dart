bc61c8fccfada57aa165b898d44dee94cc3771d6
AmirManshadi <amirmanshadi.code@gmail.com>
2026-01-03 14:31:56 +0330

    feat: add types for command and status enums with typedef for arguments

---
+++ lib/types.dart
@@ -0,0 +1,23 @@
+typedef Args = List<String?>;
+
+enum Command {
+  add('add'),
+  update('update'),
+  delete('delete'),
+  markInProgress('mark-in-progress'),
+  markDone('mark-done'),
+  list('list'),
+  unknown('unknown');
+
+  final String value;
+  const Command(this.value);
+}
+
+enum Status {
+  todo("todo"),
+  inProgress("inProgress"),
+  done("done");
+
+  final String value;
+  const Status(this.value);
+}
